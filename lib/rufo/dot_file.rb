68c7bfefa1afe617482cc9301fdb56a5c945801c
Matt Bessey <mbessey@caring.com>
2017-07-05 20:53:16 -0700

    do some benchmarking

--- lib/rufo/dot_file.rb
+++ lib/rufo/dot_file.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Rufo::DotFile
   def initialize
     @cache = {}
--- perf/benchmarking.rb
+++ perf/benchmarking.rb
@@ -1,11 +1,34 @@
 require "stackprof"
 require "flamegraph"
+require "benchmark/ips"
 require_relative "../lib/rufo"
 
-output_file = "benchmarking.html"
-Flamegraph.generate("./#{output_file}", fidelity: 0.1) do
+def test_harness
   # Benchmark with the biggest file in the repo.
   Rufo::Command.run(["./lib/rufo/formatter.rb"])
 end
 
+Benchmark.ips do |x|
+  x.config(time: 15, warmup: 1)
+
+  x.report("formatter.rb") { test_harness }
+  # TODO faster version ;)
+  # x.report("formatter.rb") { test_harness }
+
+  x.compare!
+end
+
+
+output_file = "benchmarking.html"
+Flamegraph.generate("./#{output_file}", fidelity: 0.1) do
+  test_harness
+end
 puts "Finished, results are in #{output_file}"
+
+
+# NOTES
+
+# Warming up --------------------------------------
+#         formatter.rb     1.000  i/100ms
+# Calculating -------------------------------------
+#         formatter.rb      8.331  (±12.0%) i/s -    124.000  in  15.087705s
